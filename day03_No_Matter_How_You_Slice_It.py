dc47d389a2393a59b4ec32350fef3650a5ebdbe5
Soumya Sen <ssen.mailbox@gmail.com>
2019-06-27 14:27:02 +0530

    Update day03_No_Matter_How_You_Slice_It.py

--- day03_No_Matter_How_You_Slice_It.py
+++ day03_No_Matter_How_You_Slice_It.py
@@ -1,14 +1,29 @@
-from collections import defaultdict
+from collections import Counter
 
-fabric_inch = defaultdict(int)
+fabric_inch = Counter()
+rectangles = {}
 
-with open('Data\\day03.txt','r') as f:
+with open('../input/day03.txt','r') as f:
     for line in f.read().split('\n'):
-        left, top = map(int,line.split(' ')[2][:-1].split(','))
-        x, y = map(int,line.split(' ')[3].split('x'))
-        # print(left,top,x,y)
+        items = line.split(' ')
+        id = int(items[0][1:])
+        left, top = map(int,items[2][:-1].split(','))
+        x, y = map(int,items[3].split('x'))
+        rectangles[id] = []
         for i in range(top,top+y):
             for j in range(left,left+x):
                 fabric_inch[(i,j)] += 1
+                rectangles[id].append((i,j))
 
-print(sum(1 for i in fabric_inch.values() if i>1))
+print('Square inches having 2 or more claim: ', sum(1 for i in fabric_inch.values() if i>1))
+
+
+for id in rectangles:
+    tag = True
+    for inch in rectangles[id]:
+        if fabric_inch[inch] >1:
+            tag = False
+            break
+    if tag == True:
+        print('Id of the claim that doesn\'t have overlap: ',id)
+        break
